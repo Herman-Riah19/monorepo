315d04642fe93c07ba0168cb0f95b87f9d7c8f32
Hermann <65443014+Herman-Riah19@users.noreply.github.com>
2025-03-02 14:05:05 +0300

    init the ui

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { Button } from "@repo/ui/button";
 
 export default function Home() {
   return (
@@ -14,12 +14,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+       <Button appName="docs">merci</Button>
 
         <div className={styles.ctas}>
           <a
@@ -46,9 +41,6 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="docs" className={styles.secondary}>
-          Open alert
-        </Button>
       </main>
       <footer className={styles.footer}>
         <a
